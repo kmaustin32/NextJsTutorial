9f893d1a2d7440dfde9b75f0e8ff4502326972b3
Keenan (Ezra) Austin <reaper31292@gmail.com>
2021-08-17 13:57:48 +0300

    added basic style via NextJs default method

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,27 @@
 import Link from 'next/link';
-import { render } from 'react-dom';
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout'
+
 
 export default function firstPost() {
     return (
-        <>
-        <h1>First Post!</h1>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post!</h1>
             <h2>
                 <Link href='/'>
                     <a>Return Home</a>
                 </Link>
             </h2> 
-        </>
+            <Image 
+            src='/images/profile.jpg'
+            height={144}
+            width={144} 
+            alt='Profile Image'
+            />
+        </Layout>
     )
 };
